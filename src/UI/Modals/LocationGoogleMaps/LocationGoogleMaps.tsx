d1c57311b04aa70de9033a9ef0260488d1ec10fc
essex999 <oktav2222@gmail.com>
2024-11-13 22:08:50 +0200

    fx

--- src/UI/Modals/LocationGoogleMaps/LocationGoogleMaps.tsx
+++ src/UI/Modals/LocationGoogleMaps/LocationGoogleMaps.tsx
@@ -5,9 +5,13 @@ import styles from './LocationGoogleMaps.module.scss'
 
 interface iLocationGoogleMaps {
 	onClick: () => void
+	isShowModal: boolean
 }
 
-export const LocationGoogleMaps: FC<iLocationGoogleMaps> = ({ onClick }) => {
+export const LocationGoogleMaps: FC<iLocationGoogleMaps> = ({
+	onClick,
+	isShowModal,
+}) => {
 	return (
 		<div
 			onClick={e => {
@@ -15,10 +19,12 @@ export const LocationGoogleMaps: FC<iLocationGoogleMaps> = ({ onClick }) => {
 					onClick()
 				}
 			}}
-			className={styles.wrapper}
+			className={`${styles.wrapper}  ${
+				isShowModal ? styles.modalShow : styles.modalHidden
+			}`}
 		>
 			<iframe
-				className={styles.iFrame}
+				className={` ${isShowModal ? styles.iFrame : styles.iFrameHidden}`}
 				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121214.06285525324!2d12.945962320167686!3d47.83033861156185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4776850cdf65b87b%3A0x41d25a40937b790!2s83395%20Freilassing%2C%20Germany!5e0!3m2!1sen!2s!4v1731330772850!5m2!1sen!2s'
 				loading='eager'
 			></iframe>
--- src/UI/Screens/ContactScreen/ContactScreen.tsx
+++ src/UI/Screens/ContactScreen/ContactScreen.tsx
@@ -11,56 +11,62 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 export const ContactScreen = () => {
-	const [isShowModalMap, setIsShowModalMap] = useState<boolean>(false)
+	const [isShowModal, setIsShowModal] = useState<boolean>(false)
 
 	return (
 		<div className={styles.contactWrapper}>
 			<div className={styles.leftSideBlock}>
 				<div className={styles.contactItems}>
 					<div className={styles.contactItem}>
-						<div>
+						<div className={styles.contactItemTextContent}>
 							<p>You can Email me Here</p>
-							<a href='mailto:octavian@gmail.com'>octavian@gmail.com</a>
+							<p>ixt13@yahoo.com</p>
 						</div>
-
-						<SquareIcon className={styles.icon} />
+						<a href='mailto:ixt13@yahoo.com'>
+							<SquareIcon className={styles.icon} />
+						</a>
 					</div>
 
 					<div className={styles.contactItem}>
-						<div>
+						<div className={styles.contactItemTextContent}>
 							<p>Give Me a Call on</p>
-							<a href='tel:+37368855569'>+88 888 888</a>
+							<p>+373 688 555 69</p>
 						</div>
 
-						<SquareIcon className={styles.icon} />
+						<a href='tel:+37368855569'>
+							<SquareIcon className={styles.icon} />
+						</a>
 					</div>
 
 					<div className={styles.contactItem}>
-						<div>
-							<p>Location</p>
-							<p>Chisinau , RM</p>
+						<div className={styles.contactItemTextContent}>
+							<p>My Current Location</p>
+							<p>Freilassing, Germany</p>
 						</div>
-
-						<SquareIcon
+						<a
 							onClick={() => {
-								setIsShowModalMap(true)
+								setIsShowModal(true)
 							}}
-							className={styles.icon}
-						/>
+						>
+							<SquareIcon className={styles.icon} />
+						</a>
 					</div>
 				</div>
 
 				<div className={styles.socialItems}>
 					<p>My social profiles</p>
 					<div className={styles.socialIcons}>
-						<Link href={''} target='_blank'>
+						<Link
+							href={'https://www.linkedin.com/in/octavian-ceban-96383bb6/'}
+							target='_blank'
+						>
 							<Image
 								className={styles.image}
 								src={linkedInIcon}
 								alt='socialIcon'
 							/>
 						</Link>
-						<Link href={''} target='_blank'>
+						<Link href={'https://www.facebook.com/okch97/'} target='_blank'>
 							<Image
 								className={styles.image}
 								src={facebookIcon}
@@ -78,27 +84,65 @@ export const ContactScreen = () => {
 				</div>
 			</div>
 			<div className={styles.rightSideBlock}>
-				<form action=''>
+				<p>Send me a message</p>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+					}}
+					method='post'
+				>
 					<div className={styles.inputGroup}>
-						<input type='text' placeholder='First Name' />
-						<input type='text' placeholder='Last Name' />
+						<input
+							type='text'
+							placeholder='First Name'
+							id='firstName'
+							name='firstName'
+							required
+						/>
+
+						<input
+							type='text'
+							placeholder='Last Name'
+							id='lastName'
+							name='lastName'
+							required
+						/>
+					</div>
+					<div className={styles.inputGroup}>
+						<input
+							type='email'
+							placeholder='Email'
+							id='email'
+							name='email'
+							required
+						/>
+
+						<input
+							type='number'
+							placeholder='Phone Number'
+							id='phone'
+							name='phone'
+						/>
 					</div>
 
-					<div className={styles.inputGroup}>
-						<input type='email' placeholder='Email' />
-						<input type='number' placeholder='Phone Number' />
-					</div>
-					<textarea placeholder='Your Message here...' name='' id=''></textarea>
-					<button>Send</button>
+					<textarea
+						placeholder='Your Message here...'
+						id='message'
+						name='message'
+					></textarea>
+					<button type='submit'>
+						<p>Send</p>
+						<SquareIcon className={styles.buttonIcon}></SquareIcon>
+					</button>
 				</form>
-			</div>{' '}
-			{isShowModalMap && (
-				<LocationGoogleMaps
-					onClick={() => {
-						setIsShowModalMap(false)
-					}}
-				/>
-			)}
+			</div>
+
+			<LocationGoogleMaps
+				isShowModal={isShowModal}
+				onClick={() => {
+					setIsShowModal(false)
+				}}
+			/>
 		</div>
 	)
 }
